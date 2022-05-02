a0908a1ebe6b714028b08cf7c8431acff62ad8a6
emre <emrebaglayici1@gmail.com>
2022-05-02 19:35:42 +0300

    Guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { LoginComponent } from './guest/login/login.component';
+import { RegisterComponent } from './guest/register/register.component';
+import { HomeComponent } from './guest/home/home.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { AdminComponent } from './admin/admin/admin.component';
+import { NotFoundComponent } from './error/not-found/not-found.component';
+import { UnauthorizedComponent } from './error/unauthorized/unauthorized.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    AdminComponent,
+    NotFoundComponent,
+    UnauthorizedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
